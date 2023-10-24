ed88bed86dc2f6a08f8d2b1029b7c03a67ab17d0
Jeongdong <59277907+poplar0713@users.noreply.github.com>
2023-10-24 17:31:15 +0900

    Update content.js [make selection box]

--- content.js
+++ content.js
@@ -1,87 +1,94 @@
 let isSelecting = false;
-let startX, startY, endX, endY;
-let selectionBox = null;
-
-function createSelectionBox(x, y) {
-    const box = document.createElement('div');
-    box.style.border = '2px dashed black';
-    box.style.position = 'absolute';
-    box.style.left = x + 'px';
-    box.style.top = y + 'px';
-    document.body.appendChild(box);
-    console.log('box created');
-    return box;
-}
+let win_width, win_height;
+let startX, startY;
+let darkBackground = null;
+let selectedArea = null;
 
 window.addEventListener('message', function(event) {
     if (event.data.type === "START_SELECTION") {
-        isSelecting = true;
-        console.log('capture start');
+        win_width = window.innerWidth;
+        win_height = window.innerHeight;
+
+        document.body.style.cursor = 'crosshair';
+
+        darkBackground = document.createElement("div");
+        darkBackground.id = "darkBackground";
+        darkBackground.style.borderWidth = "0 0 " + win_height + "px 0";
+
+        selectedArea = document.createElement("div");
+        selectedArea.id = "selectedArea";
+
+        document.body.appendChild(darkBackground);
+        document.body.appendChild(selectedArea);
+
+        document.body.addEventListener("mousedown", mousedown);
+        document.body.addEventListener("mousemove", mousemove);
+        document.body.addEventListener("mouseup", mouseup);
     }
 });
 
-window.addEventListener('mousedown', function (e) {
+function mousedown(e) {
+    e.preventDefault();
+    isSelecting = true;
+    startX = e.clientX;
+    startY = e.clientY;
+    console.log('mousedown', startX, startY);
+
+    document.body.removeEventListener("mousedown", mousedown);
+}
+
+function mousemove(e) {
     if(isSelecting) {
-        console.log('mouse down');
-        startX = e.pageX;
-        startX = e.pageY;
-        selectionBox = createSelectionBox(startX, startY);
+        let x = e.clientX;
+        let y = e.clientY;
+        selectedArea.style.left = x;
+        selectedArea.style.right = y;
+
+        var top = Math.min(y, startY);
+        var right = win_width - Math.max(x, startX);
+        var bottom = win_height - Math.max(y, startY);
+        var left = Math.min(x, startX);
+        darkBackground.style.borderWidth = top + 'px ' + right + 'px ' + bottom + 'px ' + left + 'px';
+        console.log('mousemove', top, right, bottom, left);
     }
-});
+}
 
-window.addEventListener('mousemove', function(e) {
-    if (!isSelecting) return;
-    if (isSelecting && selectionBox) {
-        console.log('mousemove');
-        endX = e.pageX
-        endY = e.pageY
+function mouseup(e) {
+    isSelecting = false;
+    document.body.removeEventListener("mousemove", mousemove);
+    darkBackground.parentNode.removeChild(darkBackground);
+    var x = e.clientX;
+    var y = e.clientY;
+    var top = Math.min(y, startY);
+    var left = Math.min(x, startX);
+    var width = Math.max(x, startX) - left;
+    var height = Math.max(y, startY) - top;
 
-        const rectWidth = Math.abs(endX - startX);
-        const rectHeight = Math.abs(endY - startY);
+    html2canvas(document.body).then(
+        function(canvas) { //전체 화면 캡쳐
+            // 선택 영역만큼 crop
+            var img = canvas.getContext('2d').getImageData(left, top, width, height);
+            var c = document.createElement("canvas");
+            c.width = width;
+            c.height = height;
+            c.getContext('2d').putImageData(img, 0, 0);
+            save(c);
+        }
+    );
+    document.body.style.cursor = 'auto';
+    selectedArea.parentNode.removeChild(selectedArea);
+    document.body.removeEventListener("mouseup", mouseup);
+    console.log('mouseup', width, height);
+}
 
-        selectionBox.style.left = `${Math.min(startX, endX)}px`;
-        selectionBox.style.top = `${Math.min(startY, endY)}px`;
-        selectionBox.style.width = `${rectWidth}px`;
-        selectionBox.style.height = `${rectHeight}px`;
+function save(canvas) {
+    if (navigator.msSaveBlob) {
+        var blob = canvas.msToBlob();
+        return navigator.msSaveBlob(blob, '파일명.jpg');
+    } else {
+        var el = document.getElementById("target");
+        el.href = canvas.toDataURL("image/jpeg");
+        el.download = '파일명.jpg';
+        el.click();
     }
-});
-
-window.addEventListener('mouseup', function(e) {
-    if (isSelecting && selectionBox) {
-        console.log('mouseup')
-        isSelecting = false;
-
-        // Determine the coordinates of the selected area
-        const x = Math.min(startX, endX);
-        const y = Math.min(startY, endY);
-        const width = Math.abs(startX - endX);
-        const height = Math.abs(startY - endY);
-
-        captureSelectedArea(x, y, width, height);
-
-        // Remove the selection box
-        document.body.removeChild(selectionBox);
-        selectionBox = null;
-        console.log('box destory');
-    }
-});
-
-function captureSelectedArea(x, y, width, height) {
-    html2canvas(document.body, {
-        x: x,
-        y: y,
-        width: width,
-        height: height,
-        scrollX: window.scrollX,
-        scrollY: window.scrollY,
-        windowWidth: document.documentElement.clientWidth,
-        windowHeight: document.documentElement.clientHeight
-    }).then(canvas => {
-        const dataUrl = canvas.toDataURL("image/jpg");
-        console.log('dataURL : ' + dataUrl);
-        // Send the captured image data to the background script
-        // chrome.runtime.sendMessage({ image: dataUrl }, (response) => {
-        //     console.log(response.status);
-        // });
-    });
 }
