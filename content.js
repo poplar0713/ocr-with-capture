d87fd7b89342d09532c0e466d9756ac916f7553c
Jeongdong <59277907+poplar0713@users.noreply.github.com>
2023-10-26 11:14:13 +0900

    Fixed bugs: Successful capture by reflecting the actual Y value of the browser.

--- content.js
+++ content.js
@@ -1,19 +1,16 @@
 let isSelecting = false;
-let win_width, win_height;
-let startX, startY;
+let start_clientX, start_clientY;
+let start_pageX, start_pageY;
 let darkBackground = null;
 let selectedArea = null;
 
 window.addEventListener('message', function(event) {
     if (event.data.type === "START_SELECTION") {
-        win_width = window.innerWidth;
-        win_height = window.innerHeight;
-
         document.body.style.cursor = 'crosshair';
 
         darkBackground = document.createElement("div");
         darkBackground.id = "darkBackground";
-        darkBackground.style.borderWidth = "0 0 " + win_height + "px 0";
+        darkBackground.style.borderWidth = "0 0 " + window.innerHeight + "px 0";
 
         selectedArea = document.createElement("div");
         selectedArea.id = "selectedArea";
@@ -30,9 +27,12 @@ window.addEventListener('message', function(event) {
 function mousedown(e) {
     e.preventDefault();
     isSelecting = true;
-    startX = e.clientX;
-    startY = e.clientY;
-    console.log('mousedown', startX, startY);
+    start_clientX = e.clientX;
+    start_clientY = e.clientY;
+    start_pageX = e.pageX;
+    start_pageY = e.pageY;
+    console.log('e.clientX,Y', e.clientX, e.clientY);
+    console.log('e.pageX,Y', e.pageX, e.pageY);
 
     document.body.removeEventListener("mousedown", mousedown);
 }
@@ -44,10 +44,10 @@ function mousemove(e) {
         selectedArea.style.left = x;
         selectedArea.style.right = y;
 
-        var top = Math.min(y, startY);
-        var right = win_width - Math.max(x, startX);
-        var bottom = win_height - Math.max(y, startY);
-        var left = Math.min(x, startX);
+        var top = Math.min(y, start_clientY);
+        var right = window.innerWidth - Math.max(x, start_clientX);
+        var bottom = window.innerHeight - Math.max(y, start_clientY);
+        var left = Math.min(x, start_clientX);
         darkBackground.style.borderWidth = top + 'px ' + right + 'px ' + bottom + 'px ' + left + 'px';
         console.log('mousemove', top, right, bottom, left);
     }
@@ -56,18 +56,18 @@ function mousemove(e) {
 function mouseup(e) {
     isSelecting = false;
     document.body.removeEventListener("mousemove", mousemove);
-    var x = e.clientX;
-    var y = e.clientY;
-    var top = Math.min(y, startY);
-    var left = Math.min(x, startX);
-    var width = Math.max(x, startX) - left;
-    var height = Math.max(y, startY) - top;
+    var x = e.pageX;
+    var y = e.pageY;
+    var top = Math.min(y, start_pageY);
+    var left = Math.min(x, start_pageX);
+    var width = Math.max(x, start_pageX) - left;
+    var height = Math.max(y, start_pageY) - top;
 
+    //  html2canvas는 document.body를 기준으로 캡쳐를 한다.
     html2canvas(document.body).then(
-        function(canvas) { //전체 화면 캡쳐
-            // 선택 영역만큼 crop
-            var img = canvas.getContext('2d').getImageData(left, top, width, height);
+        function(canvas) {
             var c = document.createElement("canvas");
+            var img = canvas.getContext('2d').getImageData(left, top, width, height);
             c.width = width;
             c.height = height;
             c.getContext('2d').putImageData(img, 0, 0);
